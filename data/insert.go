ba8e3e238f90a588c4b0e19b6d99df2b27210310
Berk Gokden <berkgokden@gmail.com>
2021-07-19 22:26:05 +0200

    Big Change of Using Sync.Map as Main Index

--- data/insert.go
+++ data/insert.go
@@ -3,10 +3,8 @@ package data
 import (
 	"errors"
 	"log"
-	"time"
 
 	pb "github.com/bgokden/veri/veriservice"
-	badger "github.com/dgraph-io/badger/v3"
 )
 
 // Insert inserts data to internal kv store
@@ -17,29 +15,30 @@ func (dt *Data) Insert(datum *pb.Datum, config *pb.InsertConfig) error {
 	if dt.DB == nil {
 		dt.InitData()
 	}
-	var ttlDuration *time.Duration
-	if config != nil && config.GetTTL() > 0 {
-		// log.Printf("Insert Datum with ttl config: %v\n", config.GetTTL())
-		d := time.Duration(config.GetTTL()) * time.Second
-		ttlDuration = &d
-	}
-	keyByte, err := GetKeyAsBytes(datum)
-	if err != nil {
-		return err
-	}
-	valueByte, err := GetValueAsBytes(datum)
-	if err != nil {
-		return err
-	}
-	err = dt.DB.Update(func(txn *badger.Txn) error {
-		if ttlDuration != nil {
-			// log.Printf("Insert Datum with ttl: %v\n", ttlDuration)
-			e := badger.NewEntry(keyByte, valueByte).WithTTL(*ttlDuration)
-			return txn.SetEntry(e)
-		}
-		// log.Printf("Insert Datum: %v ttl: %v\n", datum, ttlDuration)
-		return txn.Set(keyByte, valueByte)
-	})
+	err := dt.InsertBDMap(datum, config)
+	// var ttlDuration *time.Duration
+	// if config != nil && config.GetTTL() > 0 {
+	// 	// log.Printf("Insert Datum with ttl config: %v\n", config.GetTTL())
+	// 	d := time.Duration(config.GetTTL()) * time.Second
+	// 	ttlDuration = &d
+	// }
+	// keyByte, err := GetKeyAsBytes(datum)
+	// if err != nil {
+	// 	return err
+	// }
+	// valueByte, err := GetValueAsBytes(datum)
+	// if err != nil {
+	// 	return err
+	// }
+	// err = dt.DB.Update(func(txn *badger.Txn) error {
+	// 	if ttlDuration != nil {
+	// 		// log.Printf("Insert Datum with ttl: %v\n", ttlDuration)
+	// 		e := badger.NewEntry(keyByte, valueByte).WithTTL(*ttlDuration)
+	// 		return txn.SetEntry(e)
+	// 	}
+	// 	// log.Printf("Insert Datum: %v ttl: %v\n", datum, ttlDuration)
+	// 	return txn.Set(keyByte, valueByte)
+	// })
 	if err != nil {
 		return err
 	}
